a4709c971eecbab041458b04c994b142b1fe041e
Sam Rose <srose@Sams-MacBook-Pro.local>
2023-11-10 18:46:12 +0000

    Various fixes

--- content_script.js
+++ content_script.js
@@ -1,19 +1,24 @@
-const regex = /youtube.com\/(c|user|channel)/g;
+const regex = /youtube.com\/@.*/g;
 const except = [
-  'playlists',
-  'about',
-  'community',
-  'channels',
-  'about',
-  'videos'
+  "playlists",
+  "about",
+  "community",
+  "channels",
+  "about",
+  "videos",
+  "search",
 ];
 const videoTabIndex = 1;
 
-(document.body || document.documentElement).addEventListener('transitionend',
-  function(/*TransitionEvent*/ event) {
+(document.body || document.documentElement).addEventListener(
+  "transitionend",
+  function (/*TransitionEvent*/ event) {
     var url = document.location.href;
+    console.log(url, url.match(regex));
     if (url.match(regex) && !new RegExp(except.join("|")).test(url)) {
-      const channelTabs = document.getElementsByTagName("paper-tab");
-      channelTabs[videoTabIndex] && channelTabs[videoTabIndex].click();   
-}
-}, true);
+      const channelTabs = document.getElementsByTagName("yt-tab-shape");
+      channelTabs[videoTabIndex] && channelTabs[videoTabIndex].click();
+    }
+  },
+  true
+);
